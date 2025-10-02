f73108a774f4ff79e5ee4d7fcf4a3998e0efd3bd
Amarnath43 <amarnathgoshika@gmail.com>
2025-10-03 01:37:41 +0530

    UI part done final-2

--- src/components/sections/MenuSection.jsx
+++ src/components/sections/MenuSection.jsx
@@ -6,8 +6,8 @@ const MenuSection = ({ sectionRef }) => (
     <section id="menu" ref={sectionRef} className="py-14 bg-gray-800 overflow-hidden scroll-mt-30">
         <div className="container mx-auto px-4 sm:px-6 lg:px-8">
             <AnimateOnScroll className="text-center mb-16">
-                <h2 className="font-display text-4xl md:text-5xl font-bold text-white">Our Food Truck <span className="text-amber-500">Menu</span></h2>
-                <p className="text-lg mt-4 max-w-2xl mx-auto">Our menu is packed with flavor, featuring everything from breakfast Tiffins and Dosas to Bombay Chaat and our famous Truck Specials. Click to view our full menu.</p>
+                <h2 className="font-display text-4xl md:text-5xl font-bold text-white"> <span className="text-amber-500">Menu</span></h2>
+                <p className="text-lg mt-4 max-w-2xl mx-auto">Our menu is packed with flavor, featuring a wide range of authentic Indian dishes and our famous House Specials, all served in our unique outdoor setting.</p>
             </AnimateOnScroll>
             <div className="grid md:grid-cols-2 gap-8 max-w-5xl mx-auto">
                 <AnimateOnScroll className="shadow-lg">
